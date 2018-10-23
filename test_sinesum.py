84f354f8775a2cbc1afae93bf060920b265ccb4e
natanael8080 <42763537+natanael8080@users.noreply.github.com>
2018-10-23 10:55:50 -0700

    Update test_sinesum.py

--- test_sinesum.py
+++ test_sinesum.py
@@ -9,4 +9,4 @@ import sinesum
 
 def test_dummy():
     """Dummy test that always fails. Please replace with actual tests."""
-    assert False, "This is a dummy test"
+    assert n = 1, 3, 5, 10, 30, 100, "This is a dummy test"
