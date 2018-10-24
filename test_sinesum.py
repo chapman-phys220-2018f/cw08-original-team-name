78018d32570788f2518e440790a2eef5b5985e01
natanael8080 <42763537+natanael8080@users.noreply.github.com>
2018-10-23 21:05:37 -0700

    Update test_sinesum.py

--- test_sinesum.py
+++ test_sinesum.py
@@ -8,10 +8,10 @@ Verify implementation of the Fourier sine series using numpy arrays.
 import sinesum
 
 def test_dummy():
-    """Dummy test that always fails. Please replace with actual tests."""
-    assert n = 1,
-    assert n =3, 
-    assert n =5, 
-    assert n =10,
-    assert n =30, 
-    assert n =100, "This is a dummy test"
+    assert false, """Dummy test that always fails. Please replace with actual tests."""
+    #assert n = 1,
+    #assert n =3, 
+    #assert n =5, 
+    #assert n =10,
+    #assert n =30, 
+    #assert n =100, "This is a dummy test"
