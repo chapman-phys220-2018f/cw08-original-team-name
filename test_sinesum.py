d4919736f4c53f8deb0e6c5b3a2377fdc0cab53b
natanael8080 <42763537+natanael8080@users.noreply.github.com>
2018-10-25 11:07:50 -0700

    Update test_sinesum.py

--- test_sinesum.py
+++ test_sinesum.py
@@ -15,13 +15,13 @@ def test_sn():
     assert math.isclose(sinesum.sn(np.pi/2, 100000), 1.0, rel_tol=1e-5)
 
 def test_ft_2():
-    assert sinesum.func(np.pi/2) == 1
+    assert sinesum.ft(np.pi/2) == 1
 
 def test_ft_0():
-    assert sinesum.func(0) == 0
+    assert sinesum.ft(0) == 0
 
 def test_ft_2():
-    assert sinesum.func(-np.pi/2) == -1
+    assert sinesum.ft(-np.pi/2) == -1
     
 
     
