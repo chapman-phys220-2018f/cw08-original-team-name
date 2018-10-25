3e77cee2fb284cc45bd2691e0d90c7e82e3a6b45
natanael8080 <42763537+natanael8080@users.noreply.github.com>
2018-10-25 10:37:38 -0700

    Create test_sinesum.py

---
+++ test_sinesum.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+"""sinesum.py Test Module
+
+Verify implementation of the Fourier sine series using numpy arrays.
+"""
+
+import numpy.testing as np
+import sinesum
+
+
+def test_ft():
+    np.assert_approx_equal(sinesum .ft(2,0), 0.0, significant=2)
+    np.assert_approx_equal(sinesum .ft(2,0.5), 1.0, significant = 2)
+    np.assert_approx_equal(sinesum .ft(2,-0.5), -1.0, significant = 2)
+
+def test_sn():
+    np.assert_approx_equal(sinesum .sn(2,0.5,100),1, significant = 2)
+    np.assert_approx_equal(sinesum .sn(2,-0.5,100),-1, significant = 2)
+    np.assert_approx_equal(sinesum .sn(2,0,100), 0 , significant = 2)
