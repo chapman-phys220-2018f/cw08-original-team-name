18f36ed8a291294db43cc9b85f30223e9a3a7a60
natanael8080 <42763537+natanael8080@users.noreply.github.com>
2018-10-25 10:40:11 -0700

    Update test_sinesum.py

--- test_sinesum.py
+++ test_sinesum.py
@@ -11,16 +11,17 @@ import sinesum
 #def dummy_test:
 #this test wasnt that helpufl
 def test_ft():
-    np.assert_approx_equal(sinesum .ft(2,-0.5), -1.0, significant = 2)
    
     np.assert_approx_equal(sinesum .ft(2,0), 0.0, significant=2)
     
     np.assert_approx_equal(sinesum .ft(2,0.5), 1.0, significant = 2)
+    
+    np.assert_approx_equal(sinesum .ft(2,-0.5), -1.0, significant = 2)
 
 def test_sn():
-    np.assert_approx_equal(sinesum .sn(2,0,100), 0 , significant = 2)
 
     np.assert_approx_equal(sinesum .sn(2,0.5,100),1, significant = 2)
     
     np.assert_approx_equal(sinesum .sn(2,-0.5,100),-1, significant = 2)
 
+    np.assert_approx_equal(sinesum .sn(2,0,100), 0 , significant = 2)
